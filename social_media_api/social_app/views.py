f51fbe1e28933cb4a65fd12af6cdbeac019d62ed
Mark-andrew7 <kamaumark19@gmail.com>
2024-07-18 21:27:46 +0300

    Added templates and urls

--- social_media_api/social_app/views.py
+++ social_media_api/social_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse
 
 # Create your views here.
+def register(request):
+  return render(request, 'register.html')
